64d5dcd1b9c1a6a61bd5e215319b95b584898091
dackow <dackow@gmail.com>
2015-05-18 16:57:32 +0200

    Initial

---
+++ src/main/java/demo/controllers/TestController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.controllers;
+
+import demo.model.Customer;
+import demo.model.repositiories.CustomerRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.collections.IteratorUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author waldekd
+ */
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+
+    @Autowired
+    CustomerRepository repository;
+
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    String test() {
+        return "Hello from controller";
+    }
+
+    @RequestMapping(value = "/customers/names/{name}")
+    List<Customer> getCustomersByName(@PathVariable String name) {
+        return repository.findByNameLike(name);
+    }
+
+    @RequestMapping(value = "/customers/all")
+    List<Customer> getAll() {
+        Iterable<Customer> it = repository.findAll();
+        if (it != null) {
+            return IteratorUtils.toList(it.iterator());
+        } else {
+            return new ArrayList<>();
+        }
+
+    }
+
+    @RequestMapping(value = "/customers/states/{state}")
+    List<Customer> getCustomersByState(@PathVariable("state") String state1) {
+        return repository.findByState(state1);
+    }
+}
---
+++ src/main/java/demo/model/repositiories/CustomerRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo.model.repositiories;
+
+import demo.model.Customer;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Waldemar Dacko <waldekd@softsystem.pl>
+ */
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+    List<Customer> findByNameLike(String name);    
+    List<Customer> findByState(String state);
+  
+}
